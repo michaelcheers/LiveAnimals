6340457c2c3a10cc5959c5ad992d0137000f24fc
LaurieCheers <lauriecheers@gmail.com>
2016-03-31 09:29:11 +0100

    Animal graphics test

--- graphics.js
+++ graphics.js
@@ -1,16 +1,38 @@
+var GFX =
+{
+};
+
+function loadGraphics()
+{
+    var graphicsList = ["grass", "rabbit", "worm"];
+
+    graphicsList.forEach(function (name)
+    {
+        var img = new Image();
+        img.src = "img/" + name + ".png";
+        GFX[name] = img;
+    });
+}
+loadGraphics();
+
 function initGraphics()
+{
+    //    ctx.drawImage(img, 0, 0);
+    setInterval(redrawGraphics, 1000);
+}
+
+function redrawGraphics()
 {
     var canvas = document.getElementById("gameCanvas");
     var ctx = canvas.getContext("2d");
-
-    var img1 = new Image();
-
-    img1.onload = function ()
-    {
-        ctx.drawImage(img1, 0, 0);
-    };
-
-    img1.src = 'img/grass.png';
+    ctx.fillStyle = "#00FF00";
+    ctx.fillRect(0, 0, 1000, 1000);
+    ctx.drawImage(GFX["grass"], 0, 0);
+    drawAnimal(ctx, GFX["rabbit"], 100, 150);
+    drawAnimal(ctx, GFX["worm"], 250, 150);
 }
 
-setTimeout(function () { initGraphics(); }, 1000);
+function drawAnimal(ctx, image, x, y)
+{
+    ctx.drawImage(image, x-image.width*0.5, y-image.height);
+}
